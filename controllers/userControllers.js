eb5e2b0cf534baafc42b15495eb43d12412153cd
StefanoFabbro <stefano.fabbro@sancorsalud.com.ar>
2022-10-15 18:57:45 -0300

    arreglando comentarios

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -79,12 +79,10 @@ let userController = {
                     res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) * 2 })
                 }
 
-                /* console.log('estas en profile')
-                console.log(req.session);  */
+                console.log('estas en profile')
+                console.log(req.session);  
                 console.log(userToLogin);
                 
-
-                //El ID sale como "undefined", hay que cambiarlo
                 let id = userToLogin.id - 1
                 return res.redirect('/perfil/' + id)
         }
